886876a74873a52400517d4c373d8ff63a1a36d6
Shawn Rider <shawn@shawnrider.com>
2009-08-04 11:00:55 -0400

    Just getting this set up for collaboration. Pushing first commit.

--- activitystreams/handlers.py
+++ activitystreams/handlers.py
@@ -7,7 +7,7 @@ from activitystreams.models import Activity
 
 class ActivityListHandler(BaseHandler):
     allowed_methods = ('GET',)
-    fields = ('content', 'resource_uri',)
+    #fields = ('content', 'resource_uri',)
 
     def read(self, request):
         return Activity.objects.all()
@@ -15,7 +15,7 @@ class ActivityListHandler(BaseHandler):
 class ActivityHandler(BaseHandler):
     allowed_methods = ('GET', 'PUT', 'DELETE')
     fields = ('content', ('user', ('username', 'first_name')), 'resource_uri')
-    exclude = ('id', re.compile(r'^private_'))
+    #exclude = ('id', re.compile(r'^private_'))
     model = Activity
 
     def read(self, request, id):
