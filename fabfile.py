be5d4d04b6c2f879e472ab863ec637d02a6afbcc
Nowell Strite <nowell@strite.org>
2009-08-04 00:15:49 -0400

    Updated base project structure.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,27 @@
+import os
 from fabric import local
 
+PROJECT_ROOT = os.path.abspath(os.path.curdir)
+
+def bootstrap():
+    local("rm -Rf ve/")
+
+    local('virtualenv ve')
+
+    # hack activate so it uses project directory instead of ve in prompt
+    local('sed \'s/(`basename \\\\"\\$VIRTUAL_ENV\\\\\"`)/(`basename \\\\`dirname \\\\"$VIRTUAL_ENV\\\\"\\\\``)/g\' ve/bin/activate > ve/bin/activate.tmp')
+    local('mv ve/bin/activate.tmp ve/bin/activate')
+
+    # PIP install requirements
+    local("pip install -I --source=ve/src/ --environment=ve/ -r REQUIREMENTS")
+
+    # Cleanup pip install process
+    local("rm -Rf build/")
+
+    # Add local src folders to python path.
+    local("echo '%s' >> ve/lib/python2.5/site-packages/easy-install.pth" % PROJECT_ROOT)
+
 def documentation():
-    local("sphinx-build -C -D extensions=['sphinx.ext.autodoc'] -D project=django-eventlogs -D authors='Nowell Strite' -D version='0.1.0' -D release='0.1.0' -D master_doc='index' docs/ docs/_build/")
+    local("rm -Rf docs/_build/html/")
+    local("ve/bin/sphinx-build -aE docs/ docs/_build/html/")
+    print "View the documentation here: file://%s" % os.path.join(PROJECT_ROOT, 'docs', '_build', 'html', 'index.html')
