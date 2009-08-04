d8d5857329c995b5b3eecd368416cc2708ee388d
Shawn Rider <shawn@shawnrider.com>
2009-08-04 13:51:34 -0400

    Changing urls and emitters and stuff.

---
+++ activitystreams/__init__.py
@@ -0,0 +1,3 @@
+"""
+Django-Activity-Streams is an application for running a centralized activity streams server that can be shared by multiple sites. It leverages Django-Piston to provide a robust API.
+"""
--- activitystreams/handlers.py
+++ activitystreams/handlers.py
@@ -1,7 +1,7 @@
 import re
 
 from piston.handler import BaseHandler
-from piston.utils import rc, throttle
+from piston.utils import rc, throttle, require_extended
 
 from activitystreams.models import Activity
 
@@ -13,13 +13,16 @@ class ActivityListHandler(BaseHandler):
         return Activity.objects.all()
 
 class ActivityHandler(BaseHandler):
-    allowed_methods = ('GET', 'PUT', 'DELETE')
+    allowed_methods = ('GET', 'PUT', 'DELETE', 'POST')
     fields = ('content', ('user', ('username', 'first_name')), 'resource_uri')
     #exclude = ('id', re.compile(r'^private_'))
     model = Activity
 
-    def read(self, request, id):
-        activity = Activity.objects.get(pk=id)
+    def read(self, request, id=None):
+        if id:
+            activity = Activity.objects.get(pk=id)
+        else:
+            activity = Activity.objects.all() #TODO: obviously dumping all objects is bad. for testing only
         return activity
 
     @throttle(5, 10 * 60) # allow 5 times in 10 minutes
@@ -38,3 +41,21 @@ class ActivityHandler(BaseHandler):
         activity.delete()
 
         return rc.DELETED # returns HTTP 204
+
+    @require_extended
+    def create(self, request):
+        """
+        Creates a new Activity record.
+        """
+        attrs = self.flatten_dict(request.POST)
+
+        if self.exists(**attrs):
+            return rc.DUPLICATE_ENTRY
+        else:
+            action = Action.objects.get(pk=1) # TODO: TEMP FOR TESTING ONLY!
+            activity = Activity(action=action,
+                            content=attrs['content'],
+                            user=request.user)
+            activity.save()
+
+            return activity
