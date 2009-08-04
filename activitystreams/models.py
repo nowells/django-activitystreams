36ac97041dd3199972cf9e951348b3ef90790ac7
Nowell Strite <nowell@strite.org>
2009-08-04 00:56:07 -0400

    Updated to add initial data.

--- activitystreams/models.py
+++ activitystreams/models.py
@@ -11,10 +11,16 @@ from django.contrib.auth.models import User
 class Source(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __unicode__(self):
+        return self.name
+
 class Action(models.Model):
     source = models.ForeignKey(Source, related_name='actions')
     slug = models.SlugField(unique=False)
     template = models.TextField()
+
+    def __unicode__(self):
+        return u'%s: %s' % (self.source, self.slug)
 
     class Meta:
         unique_together = (('source', 'slug'),)
@@ -23,6 +29,9 @@ class ActivityObject(models.Model):
     source = models.ForeignKey(Source, related_name='activity_objects')
     content_type_id = models.PositiveIntegerField(null=True, blank=True)
     object_id = models.PositiveIntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return '%s:%s:%s' % (self.source, self.content_type_id, self.object_id)
 
     class Meta:
         unique_together = (('source', 'content_type_id', 'object_id'),)
@@ -37,6 +46,9 @@ class Activity(models.Model):
     content = models.TextField(blank=True)
 
     related_objects = models.ManyToManyField(ActivityObject, through='RelatedActivityObject')
+
+    def __unicode__(self):
+        return '%s:%s:%s' % (self.action, self.user_id, self.id)
 
 class RelatedActivityObject(models.Model):
     activity = models.ForeignKey(Activity)
